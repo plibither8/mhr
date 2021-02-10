c4067ba7fa9f723c89dc74ed89c0c11ed144fd07
Mihir Chaturvedi <plibither8@gmail.com>
2021-02-10 13:28:07 +0530

    Init: Add setup files

---
+++ src/api/_database.ts
@@ -0,0 +1,13 @@
+import low from 'lowdb'
+import FileSync from 'lowdb/adapters/FileSync'
+
+const dbPath = 'db.json'
+
+const adapter = new FileSync(dbPath)
+const database = low(adapter)
+
+/** Write initial, default content, if empty */
+const initialDb = {}
+database.defaults(initialDb).write()
+
+export default database
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import polka from 'polka'
+import database from './api/_database'
+
+polka()
+  .listen()
